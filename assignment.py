1d10270c3dc78c6d07528def5f73af0a29200f8e
Jose Gomez De La Torre <jgomez@kirby.org>
2026-09-10 14:05:55 -0700

    Remove problem statements from assignment.py
    Removed problem statements for fizz_buzz and collatz_conjecture.

--- assignment.py
+++ assignment.py
@@ -1,12 +1,6 @@
 # Type your names here: 
 
 # Problem 1
-# Write a function fizz_buzz(n) that takes an int, n. The function will count up to number n starting from 1. There are 4 rules:
-# 1. If the number is a multiple of 3, print “fizz”. 
-# 2. If the number is a multiple 5, print “buzz”. 
-# 3. If the number is a multiple of 3 and 5, print “fizzbuzz”. 
-# 4. Otherwise, print the number itself. 
-#
 # Ex. fizz_buzz(6) -> 1, 2, fizz, 4, buzz, fizz
 # Insert your code below! 
 
@@ -15,13 +9,6 @@
 
 
 # Problem 2
-# The Collatz conjecture states that if you take any positive integer, you will always eventually reach 1 by following two simple rules:
-# 1. If the number is even, divide it by 2.
-# 2. If the number is odd, multiply it by 3 and add 1.
-
-# Write a function collatz_conjecture(num) that takes an integer and runs a while loop until the integer reaches 1. 
-# At each iteration, print the number. 
-# At the end of the while loop, print the amount of steps it took to reach 1. 
 # Ex. collatz_conjecture(6)-> 6, 3, 10, 5, 16, 8, 4, 2, 1
 # Total steps: 9
 # Insert your code below!
